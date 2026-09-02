41c7085cd654e2dee3e8f188656714c2ac13ffea
zaeyee <zaeyee@163.com>
2026-09-02 11:23:11 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  pack: {
+    dts: {
+      tsgo: true,
+    },
+    exports: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {},
+});
